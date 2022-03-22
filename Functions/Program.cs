8e6b81cde4fffdad185bc7166824e2b67e03a217
Kristo Toomsoo <kristo@lteed.ee>
2022-03-22 23:49:51 +0200

    add new projects

---
+++ Functions/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta esimest arvu:");
+            int userNumberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sisesta teist arvu:");
+            int userNumberTwo = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Vali tehe (Sisesta '+', '-', '*' või '/')");
+            char userOperator = Convert.ToChar(Console.ReadLine());
+
+            switch (userOperator)
+            {
+                case '+':
+                    AddTwoNumbers(userNumberOne, userNumberTwo);
+                    break;
+                case '/':
+                    DivideTwoNumbers(userNumberOne, userNumberTwo);
+                    break;
+                case '*':
+                    MultiplyTwoNumbers(userNumberOne, userNumberTwo);
+                    break;
+                case '-':
+                    SubtractTwoNumbers(userNumberOne, userNumberTwo);
+                    break;
+                default:
+                    Console.WriteLine("Kena päeva!");
+                    break;
+            }
+        }
+
+        private static void AddTwoNumbers(int x, int y)
+        {
+            int result = x + y;
+            Console.WriteLine(result);
+        }
+
+        public static void DivideTwoNumbers(double x, double y)
+        {
+            double result = x / y;
+            Console.WriteLine(result);
+        }
+
+        public static void SubtractTwoNumbers(double x, double y)
+        {
+            double result = x - y;
+            Console.WriteLine(result);
+        }
+
+        public static void MultiplyTwoNumbers(double x, double y)
+        {
+            double result = x * y;
+            Console.WriteLine(result);
+        }
+
+        //lahutamine
+
+        //korrutamine
+    }
+}
---
+++ UserNameDataTask/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace UserNameDataTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada eesnime;
+            //programm küsib kasutajal sisestada numbrit 1-3;
+            //kui kasutaja sisestab '1', siis kuvatakse kasutaja eesnimi tagurpidi;
+            //kui kasutaja sisestab '2', siis kuvatakse kasutaja eesnime esimest tähte;
+            //kui kasutaja sisestab '3', siis kuvatakse kasutaja eesnime pikkus;
+
+            Console.WriteLine("Sisesta oma eesnimi:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Sisesta number '1', '2', '3':");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            switch (userNumber)
+            {
+                case 1:
+                    UserNameBackwards(userName);
+                    break;
+                case 2:
+                    UserNameFirstLetter(userName);
+                    break;
+                case 3:
+                    UserNameLength(userName);
+                    break;
+                default:
+                    Console.WriteLine("Proovi uuesti sisestada number 1-3");
+                    break;
+            }
+        }
+
+        public static void UserNameBackwards(string userName)
+        {
+            for (int i = userName.Length - 1; i >= 0; i--)
+            {
+                Console.Write(userName[i]);
+            }
+        }
+        public static void UserNameFirstLetter(string userName)
+        {
+            Console.WriteLine($"Sinu eesnime esimene täht on {userName[0]}");
+        }
+        public static void UserNameLength(string userName)
+        {
+            Console.WriteLine($"Sinu nimi on {userName.Length} sümbolit pikk");
+        }
+    }
+
+}
